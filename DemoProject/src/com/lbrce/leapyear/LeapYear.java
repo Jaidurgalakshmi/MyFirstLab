e5886e85ae1f2796b26c57db25cfe7182891299d
Vijaya Lakshmi <160410284+vijaya-04@users.noreply.github.com>
2024-08-22 10:44:37 +0530

    Update LeapYear.java

--- DemoProject/src/com/lbrce/leapyear/LeapYear.java
+++ DemoProject/src/com/lbrce/leapyear/LeapYear.java
@@ -4,5 +4,6 @@ public class LeapYear {
         public static void main(String args[])
         {
         	System.out.println("Welcome to DevOps lab");
+                System.out.println("Durga");
         }
 }
